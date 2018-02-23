87de11493a594b6b2a659b71443af7a71ac51bd2
Irina Karchebnaya <irenika@gmail.com>
2018-02-23 15:12:12 -0500

    optimised initialize method for the user-account-validator file

--- lib/user_account_validator.rb
+++ lib/user_account_validator.rb
@@ -4,22 +4,26 @@ require_relative 'invalid_email'
 class UserAccountValidator
 
   attr_reader :name, :username, :email, :user_hash
+  attr_accessor :errors
 
   def initialize ( user_hash )
     @user_hash = user_hash
     @name = user_hash[:name]
-
     if !username_invalid?
       @username = user_hash[:username]
     else
-      username_error
-    end
-    if !email_invalid?
-      @email = user_hash[:email]
-    else
-      email_error
+      if !email_invalid?
+        @email = user_hash[:email]
+        username_error
+      else
+        email_error
+        username_error
+      end
     end
   end
+
+  # in theory you can also append all the errors (InvalidEmail.new('Invalid email') and InvalidUserName.new('Invalide user name'))
+  # to the array @errors = [ ] and then raise them one by one @errors.each {|error| raise error}
 
   def username_invalid?
     @user_hash[:username].nil? || @user_hash[:username].empty?
